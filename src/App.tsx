af650aec44ecc63a4db92fa967ccee3d9cc1f340
ytftianwen <ytftianwen@163.com>
2022-08-03 09:56:21 +0800

    feat: vite logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import 'antd/dist/antd.css'
 import './App.css'
 import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
 import Pages from './pages/index'
-import 'antd/dist/antd.css'
 function App() {
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
